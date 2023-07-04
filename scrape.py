b64026dab12d2d8e38e24999a343724c2b7cdd95
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-07-04 18:07:05 +0500

    search result page accessed directly, extract result links

--- scrape.py
+++ scrape.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
