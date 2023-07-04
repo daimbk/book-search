3415b62a755ceaa9c9f2e8b133bbd052a51aadaa
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-07-04 18:04:43 +0500

    search page accessed directly, get result links

--- scrape.py
+++ scrape.py
@@ -2,12 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 
 # get user input of book to check
-book = input("Enter book name: ")
+book = input("Enter book name: ").title()
+author = input("Enter author's full name: ").lower()
 
-url = "https://www.readings.com.pk/"
+# result page upon searching
+url = f'https://readings.com.pk/Pages/searchresult.aspx?Keyword={book}'
 
 options = Options()
 options.add_argument("-headless")  # run Firefox in headless mode
@@ -15,31 +17,29 @@ service = Service("geckodriver-v0.33.0-win64")  # path to gecko webdriver
 driver = webdriver.Firefox(service=service, options=options)
 
 driver.get(url)
+# get page html
+html_content = driver.page_source
 
-# find the search input element
-search_input = driver.find_element(By.ID, "Search")
+# parse html with BeautifulSoup
+soup = BeautifulSoup(html_content, "html.parser")
 
-# set the value of the search input to the book name
-driver.execute_script("arguments[0].value = arguments[1];", search_input, book)
+""" find and filter the list of results """
+# find the relevant <div> elements and extract information
+div_elements = soup.find_all(
+    'div', class_='product_detail_page_left_colum_author_name')
 
-# get the JavaScript function call from the input element's onclick attribute
-onkeypress_value = search_input.get_attribute("onkeypress")
+book_links = []
+book_author = ""
+for div_element in div_elements:
+    link = div_element.find('h5').find('a')['href']  # link to book page
+    book_author = div_element.find('h6').text.strip()  # name of author
 
-if onkeypress_value:
-    # extract the JavaScript function name and parameters from the onclick value
-    function_name = onkeypress_value.split("(")[0]
-    function_params = onkeypress_value.split("(")[1].split(")")[0]
+    if book_author == author:
+        book_links.append(link)
 
-    # create the JavaScript function call with the book name as the parameter
-    javascript_call = f'{function_name}("{function_params}", "{book}");'
-
-    # execute the JavaScript function call
-    driver.execute_script(javascript_call)
-
-else:
-    print("No search function found.")
+print(book_links)
 
 # print the current URL to verify if the search was successful
-print(driver.current_url)
+# print(driver.current_url)
 
 driver.quit()
