a6304068323c6f665f7fcdca1d5a43d569346f19
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-08-09 19:52:26 +0500

    filename changed, terminal output stopped

--- scrape.py
+++ scrape.py
@@ -22,14 +22,17 @@ def check_book(book):
 
     """ find and filter the list of results """
     # find the relevant <div> elements and extract information
-    div_elements = soup.find_all('div', class_='product_detail_page_left_colum_author_name')
+    div_elements = soup.find_all(
+        'div', class_='product_detail_page_left_colum_author_name')
 
     book_links = {}
     book_author = ""
     for div_element in div_elements:
-        title = div_element.find('h5').find('a').text.strip().title()  # name of book
+        title = div_element.find('h5').find(
+            'a').text.strip().title()  # name of book
         link = div_element.find('h5').find('a')['href']  # link to book page
-        book_author = div_element.find('h6').text.strip().title()  # name of author
+        book_author = div_element.find(
+            'h6').text.strip().title()  # name of author
 
         if title == book:
             book_links[link] = book_author
@@ -40,7 +43,7 @@ def check_book(book):
         return
 
     print("Displaying all versions of the book on website.")
-    
+
     book_info = []
     # open result pages
     for page in book_links:
@@ -56,34 +59,38 @@ def check_book(book):
         soup = BeautifulSoup(response.text, "html.parser")
 
         # get book details
-        title = soup.find('div', class_='books_detail_page_left_colum_author_name').find('h5').contents[0].strip()
-        price = soup.find('div', class_='books_our_price').find('span', class_='linethrough').find_next_sibling('span')
+        title = soup.find('div', class_='books_detail_page_left_colum_author_name').find(
+            'h5').contents[0].strip()
+        price = soup.find('div', class_='books_our_price').find(
+            'span', class_='linethrough').find_next_sibling('span')
         price = price.text.strip()
 
         # check if book is available
         availability_elements = soup.find_all(class_='book_availability')
+        availability = ""
         if availability_elements:
-            print("Book is available.")
+            availability = "Book is available."
 
         else:
             # check if book is out of stock
             out_of_stock_elements = soup.find_all(class_='out_off_stock')
             if out_of_stock_elements:
-                print("Out of Stock")
+                availability = "Out of Stock"
 
             # check if available for pre-order
             else:
                 pre_order_elements = soup.find_all(class_='pree_order')
                 if pre_order_elements:
-                    print("Available for Pre-Order")
+                    availability = "Available for Pre-Order"
 
         book_dic = {"Title": title,
                     "Author": book_links[page],
                     "Price": price,
                     "Link": url}
-        
+        # implement Availability column in book_dic using availability var
+
         book_info.append(book_dic)
-    
+
     return book_info
 
 
@@ -102,29 +109,31 @@ def check_book_author(book, author):
     if response.status_code != 200:
         print("Error fetching data from the website.")
         return
-    
+
     # parse html with BeautifulSoup
     soup = BeautifulSoup(response.text, "html.parser")
     """ find and filter the list of results """
     # find the relevant <div> elements and extract information
-    div_elements = soup.find_all('div', class_='product_detail_page_left_colum_author_name')
+    div_elements = soup.find_all(
+        'div', class_='product_detail_page_left_colum_author_name')
 
     book_links = {}
     book_author = ""
     for div_element in div_elements:
-        title = div_element.find('h5').find('a').text.strip().title()  # name of book
+        title = div_element.find('h5').find(
+            'a').text.strip().title()  # name of book
         link = div_element.find('h5').find('a')['href']  # link to book page
-        book_author = div_element.find('h6').text.strip().title()  # name of author
+        book_author = div_element.find(
+            'h6').text.strip().title()  # name of author
 
         if book_author == author:
             book_links[link] = book_author
 
-    
     # exit if no book is found
     if len(book_links) == 0:
         print("No books of such name found.")
         return
-    
+
     print("Displaying all versions of the book on website.")
 
     book_info = []
@@ -136,44 +145,46 @@ def check_book_author(book, author):
 
         # Fetch the new page content
         response = requests.get(url)
-        
+
         if response.status_code != 200:
             print("Error fetching data from the website.")
             continue
-        
 
         # parse the new page with BeautifulSoup
         soup = BeautifulSoup(response.text, "html.parser")
         # get book details
-        title = soup.find('div', class_='books_detail_page_left_colum_author_name').find('h5').contents[0].strip()
-        price = soup.find('div', class_='books_our_price').find('span', class_='linethrough').find_next_sibling('span')
+        title = soup.find('div', class_='books_detail_page_left_colum_author_name').find(
+            'h5').contents[0].strip()
+        price = soup.find('div', class_='books_our_price').find(
+            'span', class_='linethrough').find_next_sibling('span')
         price = price.text.strip()
 
         # check if book is available
         availability_elements = soup.find_all(class_='book_availability')
+        availability = ""
         if availability_elements:
-            print("Book is available.")
+            availability = "Book is available."
 
         else:
             # check if book is out of stock
             out_of_stock_elements = soup.find_all(class_='out_off_stock')
             if out_of_stock_elements:
-                print("Out of Stock")
+                availability = "Out of Stock"
 
             # check if available for pre-order
             else:
                 pre_order_elements = soup.find_all(class_='pree_order')
                 if pre_order_elements:
-                    print("Available for Pre-Order")
+                    availability = "Available for Pre-Order"
 
         book_dic = {"Title": title,
                     "Author": book_links[page],
                     "Price": price,
                     "Link": "https://readings.com.pk" + page}
-        
+        # implement Availability column in book_dic using availability var
+
         book_info.append(book_dic)
-    
-        
+
         # provide option to open link in browser
         # open_link(page)
 
@@ -181,7 +192,7 @@ def check_book_author(book, author):
 
 
 def func_options(book, author):
-    if author == "":    
+    if author == "":
         # call scraping script to check book availability using book title
         return check_book(book.title())
 
